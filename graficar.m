a33d994eec9660de8648a520eea1c50ff435b4e2
DavidHernandezP <77238574+DavidHernandezP@users.noreply.github.com>
2021-12-14 10:56:27 +0100

    Script en Matlab para graficar las topologías

---
+++ graficar.m
@@ -0,0 +1,22 @@
+%extraer datos de los nodos y enlaces
+nodes = importdata('Nodos.txt');
+edges = importdata("Enlaces.txt");
+
+%posicion nodos
+posX_nodes = nodes(:,2)';
+posY_nodes = nodes(:,3)';
+
+from_node = edges(:,1)' + 1; 
+to_node = edges(:,2)' + 1;
+% '+1' es porque BRITE empieza a numerar por el nodo 0 y matlab empieza
+% por el 1
+
+g = graph(from_node,to_node);
+plot(g, 'XData', posX_nodes, 'YData', posY_nodes);
+
+
+
+
+
+
+
